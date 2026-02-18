76d7f2f4f33e0be3e0abefaf1728a811cbeed9ce
Henry <henry.zoellner@gmail.com>
2026-02-18 01:15:54 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_URL="https://github.com/YOUR_USERNAME/nvim-config.git"
+NVIM_DIR="$HOME/.config/nvim"
+
+# Backup existing config if it exists
+if [ -d "$NVIM_DIR" ]; then
+  echo "Backing up existing nvim config..."
+  mv "$NVIM_DIR" "$NVIM_DIR.backup.$(date +%s)"
+fi
+
+echo "Cloning nvim config..."
+git clone "$REPO_URL" "$NVIM_DIR"
+
+echo "Done."
